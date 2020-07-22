c40e7d1387633ad585e18c2e122f74a946c62fff
Yann Depps <yann.deperrot@protonmail.com>
2020-07-22 20:51:34 +0200

    config :fu:

--- doom/config.el
+++ doom/config.el
@@ -64,24 +64,24 @@
 (after! org
   (setq org-bullets-bullet-list '("∵")))
 
-(def-package! org-super-agenda
-  :after org-agenda
-  :init
-  (setq org-super-agenda-groups '((:name "Today"
-                                         :time-grid t
-                                         :scheduled today)
-                                  (:name "Due today"
-                                         :deadline today)
-                                  (:name "Important"
-                                         :priority "A")
-                                  (:name "Overdue"
-                                         :deadline past)
-                                  (:name "Due soon"
-                                         :deadline future)
-                                  (:name "Big Outcomes"
-                                         :tag "bo")))
-  :config
-  (org-super-agenda-mode))
+;; (def-package! org-super-agenda
+;;   :after org-agenda
+;;   :init
+;;   (setq org-super-agenda-groups '((:name "Today"
+;;                                          :time-grid t
+;;                                          :scheduled today)
+;;                                   (:name "Due today"
+;;                                          :deadline today)
+;;                                   (:name "Important"
+;;                                          :priority "A")
+;;                                   (:name "Overdue"
+;;                                          :deadline past)
+;;                                   (:name "Due soon"
+;;                                          :deadline future)
+;;                                   (:name "Big Outcomes"
+;;                                          :tag "bo")))
+;;   :config
+;;   (org-super-agenda-mode))
 
 
 
