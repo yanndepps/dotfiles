5cd6224f6659e8de00cb6a2808a460fe1cef868b
yanndepps <yann.deperrot@protonmail.com>
2023-07-22 15:55:06 +0200

    doom init

--- doom/+ui.el
+++
@@ -1,6 +0,0 @@
-;;;  -*- lexical-binding: t; -*-
-;; nov.el
-(defun my-nov-font-setup ()
-  (face-remap-add-relative 'variable-pitch :family "JetBrains Mono"
-                                           :size 12))
-(add-hook 'nov-mode-hook 'my-nov-font-setup)
